3c89bf5d0c5e4c733e59626b35eb6288d13c8c56
Wade <wsw0615@gmail.com>
2021-05-06 23:05:43 +0800

    feat: node express

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -5,15 +5,24 @@ import VueAxios from 'vue-axios';
 Vue.use(VueAxios, axios);
 
 const lolapi = {
-  get_teams: async () => (
-    Vue.axios({
-      method: 'get',
-      baseURL: 'https://fly.sportsdata.io/v3/lol/scores/json',
-      url: '/Teams',
-      'Content-Type': 'application/json',
-      'Ocp-Apim-Subscription-Key': 'e1ed7a271abe4b569d408758dec7dafb',
-    }).then((response) => response)
-  ),
+  get_teams: async () => {
+    // axios({
+    //   method: 'get',
+    //   baseURL: 'https://fly.sportsdata.io/v3/lol/scores/json',
+    //   url: '/Teams',
+    //   'Content-Type': 'application/json',
+    //   'Ocp-Apim-Subscription-Key': 'e1ed7a271abe4b569d408758dec7dafb',
+    // }).then((response) => {
+    //   console.log(response);
+    //   return response;
+    // { headers: { 'Ocp-Apim-Subscription-Key': 'e1ed7a271abe4b569d408758dec7dafb',
+    // 'Content-Type': 'application/json' } })
+    // })
+    axios.get('https://cors-anywhere.herokuapp.com/https://sportsdata.io/developers/api-documentation/lol#/base/areas-countries?key=e1ed7a271abe4b569d408758dec7dafb')
+      .then((response) => {
+        console.log(response);
+      });
+  },
   async get_test() {
     return 'test success';
   },
