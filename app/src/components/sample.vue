212d08bf8119eed1b4acb4ec446ac6a3c5889521
Wade <wsw0615@gmail.com>
2021-05-13 17:40:50 +0800

    feat: sample.vue

---
+++ app/src/components/sample.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="??">
+    Sample
+  </div>
+</template>
+
+<script>
+export default {
+  props: [''],
+  watch: {
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted() {
+  },
+  beforeMount() {
+  },
+  updated() {},
+};
+</script>
+
+<style rel="stylesheet/sass" lang="sass" scoped>
+#??
+  position: relative
+</style>
+
+// 使用方式
+// <?? />
